9139541bc34d08a00e3e62900e9bfbb966d1035b
Davronbek Abdurazzokov <abdurazzoqovdavronbek3@gmail.com>
2024-02-26 22:39:25 +0300

    Home  va About divlarimizning o'lchamlarini ekran kichikligiga ko'ra togirlash ishlari bajarildi

--- JS/AboutMeJS.js
+++ JS/AboutMeJS.js
@@ -24,13 +24,12 @@
 		let myVar = setInterval(myTimer, 100);
 		function myTimer() {
 		  	var h = window.innerHeight;
-		  	document.getElementsByClassName("main-div")[0].style.height = h+"px" ;
+		  	
 		  	document.getElementsByClassName("main-div")[1].style.height = h+"px" ;
 			document.getElementById("contactMe").style.height = h+"px" ;
-			
-			document.getElementById("contactMeFormLeftDiv").style.height = (h/100)*60 - 60 +"px"; 
+			//document.getElementById("contactMeFormLeftDiv").style.height = (h/100)*60 - 60 +"px"; 
 			document.getElementById("hrContact").style.height = (h/100)*60 - 60 +"px";
-			document.getElementById("contactMeFormRightDiv").style.height = (h/100)*60 - 60 +"px";
+			//document.getElementById("contactMeFormRightDiv").style.height = (h/100)*60 - 60 +"px";
 		}
 
 		
@@ -77,9 +76,8 @@
 
 		//dil tanlovi slector va Glob icon tugma birlashtirilishi
 		function dilSelector() {
-			document.getElementById("dilList").style.display = "";
+			document.getElementById("dilList").style.display = "flex";
 		}
-
 		
 		function fonIshla(x) {
 			x.classList.add("icon-img-account-2");
@@ -88,38 +86,50 @@
 			x.classList.remove("icon-img-account-2");
 		}
 
-
 // Ekran eni 1028pxdan kichik bo'lganda ekrandaki elementlarning joylashishi va o'zgarishi
-
-
 	let myVar1028 = setInterval(myTimer1028, 100);
 	function myTimer1028() {
+
     	var w= window.innerWidth;
     	var h = window.innerHeight;
 	    if(w < 1028){
 	    	//skils div
 		  	document.getElementById("my-skills-h1").style.paddingTop = "0px";
 		  	document.getElementById("portfolioImg").style.paddingLeft = "110px";
-		  	
 
 		  	//contact Me div
 		  	document.getElementById("contactMeFormLeftDiv").style.float = "none";
-		  	document.getElementById("contactMeFormLeftDiv").style.width = "85%"
-		  	document.getElementById("contactMeFormRightDiv").style.width = "85%"
-		  	document.getElementById("contactMeFormRightDiv").style.paddingTop = "0px"
+		  	document.getElementById("contactMeFormLeftDiv").style.width = "85%";
+		  	document.getElementById("contactMeFormRightDiv").style.width = "85%";
+		  	document.getElementById("contactMeFormRightDiv").style.paddingTop = "0px";
 		  	document.getElementById("contactMeFormLeftDiv").style.paddingTop = "50px";
 		  	document.getElementById("hrContact").style.display = "none";
-		  	document.getElementById("contactMeFormDiv").style.height = "800px";
+		  	document.getElementById("contactMeFormDiv").style.height = "800px";	
+		  	
 	 	}else{
 	 		document.getElementById("portfolioImg").style.paddingLeft = "";
 
-
 	 		//contact Me div
-	 		document.getElementById("contactMeFormLeftDiv").style.width = "40%"
-	 		document.getElementById("contactMeFormRightDiv").style.width = "40%"
+	 		document.getElementById("contactMeFormLeftDiv").style.width = "40%";
+	 		document.getElementById("contactMeFormRightDiv").style.width = "40%";
 	 		document.getElementById("contactMeFormDiv").style.height = (h/100)*60 +"px";
 		  	document.getElementById("contactMeFormLeftDiv").style.float = "left";
-		  	document.getElementById("contactMeFormRightDiv").style.paddingTop = "50px"
+		  	document.getElementById("contactMeFormRightDiv").style.paddingTop = "0px";
+		  	document.getElementById("contactMeFormLeftDiv").style.paddingTop = "0px";
 		  	document.getElementById("hrContact").style.display = "";
+	
 		  	}
+		
+		if(w < 816){
+			//About And Home Div
+		  	document.getElementById("main-body-text").style.paddingTop = "120px";
+		  	document.getElementById("main-body-text").style.paddingLeft = "0px";
+		  	document.getElementsByClassName("main-div")[0].style.height = h+210+"px" ;
+		}else{
+			//About And Home Div
+			document.getElementsByClassName("main-div")[0].style.height = h+"px" ;
+		  	document.getElementById("main-body-text").style.paddingTop = "150px";
+		  	document.getElementById("main-body-text").style.paddingLeft = "30px";
+		  	
 		}
+	}
